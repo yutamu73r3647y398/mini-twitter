d9400befc05359c68fdbfaf908e65f39c236082a
yutamu <iwamoto.yuta1925@gmail.com>
2023-07-06 02:56:40 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  root "application#hello"
+  # ルート「/」へのGETリクエストをStaticPagesコントローラのhomeアクションにルーティングさせる
+  root "static_pages#home"
+#   contorollerのhome,helpにルーティング、GETで設定
+  get  "static_pages/home"
+  get  "static_pages/help"
+  get  "static_pages/about"
+  get  "static_pages/contact"
 end
