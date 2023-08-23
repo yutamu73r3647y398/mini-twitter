fe3cee8f9d61259caa43cbe81669427cf4ea067a
yutamu <iwamoto.yuta1925@gmail.com>
2023-08-23 00:56:57 +0000

    binの設定

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rails db:migrate:reset
+bundle exec rails db:seed
 # DBリセット用
 # bundle exec rails db:migrate:reset
